a0f669414c4018824ea3c0e059103ef4232ea223
Gabriel Martín Moran <moran.gabriel.95@gmail.com>
2021-05-26 22:26:20 -0300

    Agregada base para sistema de sed

--- spigot_server/debug.bat
+++ spigot_server/debug.bat
@@ -1,2 +1,2 @@
-java -Xms3G -Xmx3G -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar spigot-1.16.5.jar nogui
+java -Xms1G -Xmx1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar spigot-1.16.5.jar nogui
 pause
